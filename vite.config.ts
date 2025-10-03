01b28c5c0276ec59463e47ad4ef5148d175335a2
ayaSaleh717 <105351734+ayaSaleh717@users.noreply.github.com>
2025-10-03 14:13:39 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
+  base: '/rzn_chatbot_/', // ✅ أضف هذا السطر
   plugins: [react()],
   resolve: {
     alias: {
